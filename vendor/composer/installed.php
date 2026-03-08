fde48aa1d188116d5721a253bcc42c339f0b388e
7uhr12 <7uhr12@gmail.com>
2026-03-08 16:59:50 +0000

    composed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '7uhr12/myauthlib',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '591ec7ce8428752cb54e1b31e0c8ba43b20867e5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '7uhr12/myauthlib' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '591ec7ce8428752cb54e1b31e0c8ba43b20867e5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
